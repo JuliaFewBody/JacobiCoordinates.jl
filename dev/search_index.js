8a904e58958fd90913711868b8f9e33f447eacaa
Documenter.jl <documenter@juliadocs.github.io>
2025-11-02 06:41:15 +0000

    build based on ef715e3

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#JacobiCoordinates","page":"Home","title":"JacobiCoordinates","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for JacobiCoordinates.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
